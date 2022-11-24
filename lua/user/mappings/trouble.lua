408207b8b626069a8d0b2e92c7f95a1cd95fe7a0
stengah <deepsghimire1@gmail.com>
2022-11-24 20:37:30 +0545

    trouble keybinding documentation

--- lua/user/mappings/trouble.lua
+++ lua/user/mappings/trouble.lua
@@ -1,18 +1,39 @@
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+
+local wk = require('which-key')
+
+
+vim.keymap.set("n", "<leader>qq", "<cmd>TroubleToggle<cr>",
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+vim.keymap.set("n", "<leader>qw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+vim.keymap.set("n", "<leader>qd", "<cmd>TroubleToggle document_diagnostics<cr>",
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+vim.keymap.set("n", "<leader>ql", "<cmd>TroubleToggle loclist<cr>",
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+vim.keymap.set("n", "<leader>qf", "<cmd>TroubleToggle quickfix<cr>",
   { silent = true, noremap = true }
 )
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-  { silent = true, noremap = true }
+  { silent = true, noremap = true ,desc = "lsp references"}
 )
+
+opts = {
+    prefix="<leader>"
+}
+
+mapping_doc = {
+    q = {
+        name = "quickfix list",
+        q = "Toggle",
+        w = "Workspace diagnostics",
+        d = "Document diagnostics",
+        l = "location list",
+        f = "quickfix list",
+    }
+}
+
+wk.register(mapping_doc,opts)
