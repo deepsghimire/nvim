604d34ef9e249573f0e7cb10a25bb62fdee8b95e
stengah <deepsghimire1@gmail.com>
2023-06-04 16:30:02 +0545

    move lines up and down selection is not working for now

--- lua/user/mappings/init.lua
+++ lua/user/mappings/init.lua
@@ -3,7 +3,20 @@ require("user.mappings.diagnostics")
 require("user.mappings.trouble")
 require("user.mappings.snippets")
 require("user.mappings.nvim-tree")
-require('user.mappings.gitsigns')
+require("user.mappings.gitsigns")
 
+vim.keymap.set({ "n" }, "<C-n>", function()
+	vim.cmd.move(".+<CR>==")
+end, { noremap = true, desc = "move line down" })
 
+vim.keymap.set({ "n" }, "<C-p>", function()
+	vim.cmd.move(".-2<CR>==")
+end, { noremap = true, desc = "move live up" })
 
+vim.keymap.set({ "v" }, "<C-n>", function()
+	vim.cmd.move("'>+1<CR>gv=gv")
+end, { noremap = true, desc = "move selection down" })
+
+vim.keymap.set({ "v" }, "<C-p>", function()
+	vim.cmd.move("'<-2<CR>gv=gv")
+end, { noremap = true, desc = "move selection up" })
